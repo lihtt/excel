7b2157d91cb916ff00a76b51d26aa23affe4e724
lihtt <163462006+lihtt@users.noreply.github.com>
2024-08-11 23:54:42 +0500

    Framework start (#2)
    * finish base framework functional
    
    * comment debug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+import {Excel} from '@/components/excel/Excel'
+import {Header} from '@/components/header/Header'
+import {Toolbar} from '@/components/toolbar/Toolbar'
+import {Formula} from '@/components/formula/Formula'
+import {Table} from '@/components/table/Table'
+
 import './scss/index.scss'
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-console.log('Working! hard')
+console.log('Stay hard!')
+
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+})
+
+excel.render()
+
+
